4167c353bb13f907b5dff2a227ad3a4535f5cab5
Yiting Zheng <yzhe0132@student.monash.edu>
2024-08-22 18:15:35 +1000

    completed activity 5

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="about">
-      <h1>About Our Library</h1>
-      <p>Welcome to our digital library! We're dedicated to providing a vast collection of books and resources to our community.</p>
+    <div class="about mt-5">
+        <div class="row">
+          <div class="col-md-8 offset-md-2">
+            <h1 class="text-center">About Our Library</h1>
+            <p class="text-center">Welcome to our digital library! We're dedicated to providing a vast collection of books and resources to our community.</p>
+          </div>
+        </div>
     </div>
   </template>
   
