0bd2fd322854e57014b2a6c6d671aab721def60b
Yiting Zheng <yzhe0132@student.monash.edu>
2024-10-01 11:40:14 +1000

    first commit

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
+// import './assets/main.css'
+// import '@/assets/style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
+// import DataTable from 'primevue/datatable'
+// import Column from 'primevue/Column'
 
-// import './style.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+const app = createApp(App)
+app.use(PrimeVue, { theme: { preset: Aura } })
 
-const app = createApp(App);
-app.use(PrimeVue, {
-    theme: {
-        preset: Aura
-    }
-})
+// app.component('DataTable', DataTable)
+// app.component('Column', Column)
 
-createApp(App).mount('#app')
+app.mount('#app')
