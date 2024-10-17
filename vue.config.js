e5c44c466a90b439757db1e34bf11d3240d090f0
Yiting Zheng <yzhe0132@student.monash.edu>
2024-10-18 02:51:51 +1100

    tj

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/yzheng-library/' // 使用 GitHub Pages 项目的子路径
+      ? '/' // 使用 GitHub Pages 项目的子路径
       : '/'
   }
   
